8a53f17c7bb8bf88535700f31cbe8b2cfc115f61
Douglas Pires <douglas.pires@live.com>
2020-03-11 22:46:41 -0300

    test

--- api/palette-picker.js
+++ api/palette-picker.js
@@ -1,8 +1,22 @@
 // http://colormind.io/api-access/
-import express from 'express'
+// import express from 'express'
 import 'isomorphic-fetch'
-const app = express()
+// const app = express()
 
+async function app(req, res, next) {
+  const data = await fetch('http://colormind.io/api/', {
+    method: 'POST',
+    body: JSON.stringify({
+      model: 'default'
+    })
+  })
+    .then(result => result.json())
+    .catch(console.log)
+  console.log(data)
+  console.log(res)
+  res = data
+  next()
+}
 // app.get('/', async (req, res) => {
 //   const data = await fetch('http://colormind.io/api/', {
 //     method: 'POST',
